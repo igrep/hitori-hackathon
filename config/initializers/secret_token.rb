914916471d79624d0a2dc5216b8652bd946b8222
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-11-25 11:56:10 +0900

    いろいろ追加

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '00efcf34d821d5e26f9dcc7ac6380286c822cc216da05bdecb0e11ef4c3f247712580140e7fd8b7df031ba41e5ce46bede38ed6dec12cf59c46e5192a4bbc6be'
