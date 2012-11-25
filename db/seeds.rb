5d2425f381b169745c64b3c6dcd69c72dcf39a65
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-11-25 13:32:15 +0900

    Tasksのseedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# http://www.oiax.jp/rails/rails3/database_table_and_model.html
+# からパクったかのように見えるコード
+
+table_names = %w{ tasks }
+
+table_names.each do |table_name|
+  tasks_path = "#{Rails.root}/db/seeds/#{Rails.env}/#{table_name}"
+  require tasks_path if File.exist? tasks_path
+end
---
+++ db/seeds/development/tasks.rb
@@ -0,0 +1,45 @@
+Task.create(
+  name: "フレッシュプリキュアを見る",
+  due_time: Time.now + 1.week,
+  status: "not yet"
+)
+Task.create(
+  name: "ひだまりスケッチを見る",
+  due_time: Time.now + 3.days,
+  status: "not yet"
+)
+Task.create(
+  name: "Hakellの問題を作る",
+  due_time: nil,
+  status: "done"
+)
+Task.create(
+  name: "もっと強くなる",
+  due_time: nil,
+  status: "doing"
+)
+Task.create(
+  name: "ひとりハッカソンをとにかくやれ",
+  due_time: Time.now,
+  status: "doing"
+)
+Task.create(
+  name: "ブログシステムを変える",
+  due_time: Time.now + 3.weeks,
+  status: "not yet"
+)
+Task.create(
+  name: "Haskell Advent Calendar",
+  due_time: Time.parse("2012/12/03"),
+  status: "not yet"
+)
+Task.create(
+  name: "ML/LL Advent Calendar",
+  due_time: Time.parse("2012/12/22"),
+  status: "not yet"
+)
+Task.create(
+  name: "Vim Advent Calendar",
+  due_time: Time.parse("2012/12/29"),
+  status: "not yet"
+)
