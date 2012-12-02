4785f7c3a4489bb99f2e9cd35a689dc2c5f518f7
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-12-02 15:43:53 +0900

    タスクを追加した後追加したタスクが表示されるかどうかテストした

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,10 +3,16 @@ require 'spec_helper'
 describe "Tasks" do
   describe "GET /" do
     it "has form to add a task" do
-      new_task = { name: 'test task' }
+      new_task = 'test task'
       visit root_path
-      fill_in 'task_name', with: new_task[:name]
+      fill_in 'task_name', with: new_task
       expect { click_button 'Create Task' }.to change(Task, :count).by(1)
+
+      # 他に既にタスクがある場合を想定すると適切じゃない。。。
+      response.should have_selector('.task_property', content: new_task )
+      response.should have_selector(
+        '.task_property', content: Task::STATUS_NOT_YET
+      )
     end
   end
 end
