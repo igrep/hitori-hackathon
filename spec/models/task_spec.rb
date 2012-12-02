283ba9f311f6be2d01db9e514eb0b971477e9e48
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-12-02 14:49:36 +0900

    Modelのテストを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Task do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can add a non-blank task' do
+    added_task = Task.add( name: 'Test Task', due_time: Time.now )
+    added_task.should be_valid
+  end
+
+  it 'can not add blank task' do
+    added_task = Task.add( name: '', due_time: Time.now )
+    added_task.should_not be_valid
+  end
+
+  it 'adds a task whose initial status is Task::STATUS_NOT_YET' do
+    added_task = Task.add( name: 'Test Task', due_time: Time.now )
+    added_task.status.should == Task::STATUS_NOT_YET
+  end
 end
