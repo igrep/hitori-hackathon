16e3cc686568b155f8269245e2ef24552180de33
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-12-02 12:34:02 +0900

    TaskControllerのテスト追加。でも実際にはviewのテストとなってるわけだが...

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,11 +1,34 @@
 require 'spec_helper'
 
 describe TasksController do
+  render_views
+
+  describe "GET '/'" do
+    it "returns http success" do
+      get '/'
+      response.should be_success
+    end
+  end
 
   describe "GET 'list'" do
-    it "returns http success" do
+    before :all do
       get 'list'
+    end
+    it "returns http success" do
       response.should be_success
+    end
+
+    it 'has task list' do
+      #そもそもテーブルでないといけない理由はないけど...
+      response.should have_selector('table#task_list')
+      # タスクが0この場合も考えてとりあえずthのみテスト
+      response.should have_selector('th.task_header')
+    end
+
+    it 'has a form to post a new task' do
+      response.should have_selector('form')
+      # 現状最低限テストしたいのはタスクの名前と期限だけど期限はどうテストしようか
+      response.should have_selector('input', name: 'task[name]')
     end
   end
 
