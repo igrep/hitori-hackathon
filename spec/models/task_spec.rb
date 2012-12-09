da3861a5442e29a6404a25783476dcaede5ba790
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-12-09 17:44:46 +0900

    http://d.hatena.ne.jp/t-wada/20100228/p1 を参考にtask_specを書き換え

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,23 +1,21 @@
 require 'spec_helper'
 
-describe Task do
-  describe 'when adding a task' do
-    before :each do
-      @added_task = Task.add( name: 'Test Task', due_time: Time.now )
-    end
+describe Task, 'when adding a task' do
 
-    it 'the task is added to the last ' do
-      @added_task.should be_valid
-      Task.last.should == @added_task
-    end
+  subject { Task.add( name: 'Test Task', due_time: Time.now ) }
 
-    it "the task name cannot be blank" do
-      @added_task.name = ''
-      @added_task.should_not be_valid
-    end
+  # Task.countの増減をテストするにはこの場合どうすればよいものか。
+  it 'adds the new task to the last' do
+    should be_valid
+    should == Task.last
+  end
 
-    it "the added task's status is Task::STATUS_NOT_YET" do
-      @added_task.status.should == Task::STATUS_NOT_YET
-    end
+  it "can't add a task whose name is blank" do
+    subject.name = ''
+    should_not be_valid
+  end
+
+  it "adds a task whose status is Task::STATUS_NOT_YET" do
+    subject.status.should == Task::STATUS_NOT_YET
   end
 end
