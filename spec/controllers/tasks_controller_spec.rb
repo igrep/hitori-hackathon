20ed3555110940998a2d735ddd7a5b509b9cb73a
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-12-09 17:10:46 +0900

    controllerにPOSTのテストを追加したけど実質Modelのテストになってしまった

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -4,10 +4,9 @@ describe TasksController do
   # これは消してhtmlのテストはviewに置くべき
   render_views
 
-  # postするテストも追加したいなあ
   describe "GET 'list'" do
     before :each do
-      get 'list'
+      get :list
     end
     it "returns http success" do
       response.should be_success
@@ -25,6 +24,29 @@ describe TasksController do
       # 現状最低限テストしたいのはタスクの名前
       # 期限はいろいろな入力フォームがありえるので保留
       response.should have_selector('input', name: 'task[name]')
+    end
+  end
+
+  # 実質Modelのテストになってる... orz
+  describe "POST '/tasks/'" do
+    context 'in posting valid parameters,' do
+      it 'increases the number of tasks' do
+        expect {
+          post :add, task: { name: 'test', due_time: Time.now }
+        }.to change(Task, :count).by(1)
+      end
+
+      it 'increases the number of tasks without due_time' do
+        expect {
+          post :add, task: { name: 'test' }
+        }.to change(Task, :count).by(1)
+      end
+    end
+
+    it 'in posting invalid parameters, doesn\'t increase the number of tasks' do
+      expect {
+        post :add, task: { name: '', due_time: Time.now }
+      }.not_to change(Task, :count).by(1)
     end
   end
 
