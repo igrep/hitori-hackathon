09f383bc67dd7e94e60eb31165b0693810b840ed
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-12-23 14:29:52 +0900

    add done? method

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,8 +5,12 @@ class Task < ActiveRecord::Base
   PROPERTY_NAMES = {
     name: 'Task',
     due_time: 'Deadline',
-    done: 'Status'
+    done: 'Done?'
   }
+
+  def done?
+    self.done
+  end
 
   def self.add_unfinished params
     self.create params.merge( done: false )
