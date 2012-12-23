49f3685d7f2d8ec400860463e4d9cab0ef2e06f9
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-12-23 16:55:49 +0900

    simpler request/tasks spec

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 describe "Tasks" do
-  describe "GET /" do
-    it "has form to add a task" do
-      new_task = "test task #{rand}"
-      visit root_path
-      fill_in 'task_name', with: new_task
-      expect { click_button 'Create Task' }.to change(Task, :count).by(1)
+  it "has form to add a task" do
+    new_task = "test task #{rand}"
+    visit root_path
+    fill_in 'task_name', with: new_task
+    expect { click_button 'Create Task' }.to change(Task, :count).by(1)
 
-      response.should have_selector(
-        '.task_property', content: new_task
-      )
-    end
+    response.should have_selector(
+      '.task_property', content: new_task
+    )
   end
 end
