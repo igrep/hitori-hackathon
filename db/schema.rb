bac29ea30e8008f7c5b119ed71e054c42db51341
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-12-23 14:13:08 +0900

    remove status column

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121223035741) do
+ActiveRecord::Schema.define(:version => 20121223050826) do
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
     t.datetime "due_time"
-    t.string   "status"
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
     t.boolean  "done",       :default => false
