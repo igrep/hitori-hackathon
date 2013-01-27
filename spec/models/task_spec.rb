cdbe698f10cb28c849c7471f0f104245693e69ff
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2013-01-27 18:56:10 +0900

    defining spec of Task.from_kvs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,31 +1,57 @@
 require 'spec_helper'
 
-describe Task, 'when adding a task,' do
+describe Task do
+  context 'when adding a task,' do
 
-  subject { Task.add_unfinished( name: 'Test Task', due_time: Time.now ) }
+    subject { Task.add_unfinished( name: 'Test Task', due_time: Time.now ) }
 
-  # Task.countの増減をテストするにはこの場合どうすればよいものか。
-  # とりあえずcontrollerのテストでやってるからいいか。
-  it 'adds the new task to the last' do
-    should be_valid
-    should == Task.last
+    # Task.countの増減をテストするにはこの場合どうすればよいものか。
+    # とりあえずcontrollerのテストでやってるからいいか。
+    it 'adds the new task to the last' do
+      should be_valid
+      should == Task.last
+    end
+
+    it "can't add a task whose name is blank" do
+      subject.name = ''
+      should_not be_valid
+    end
+
+    it "adds a task which is not done" do
+      subject.should_not be_done
+    end
+
+    it 'adds a task with its description' do
+      should be_respond_to( :description )
+    end
+
+    it 'adds a task whose description can be assigned' do
+      subject.description = 'This is a task for testing'
+      should be_valid
+    end
   end
 
-  it "can't add a task whose name is blank" do
-    subject.name = ''
-    should_not be_valid
-  end
+  context 'when adding by add_from_kvs' do
+    before { Task.delete_all }
 
-  it "adds a task which is not done" do
-    subject.should_not be_done
-  end
+    before :all do
+      @kvs_tasks = {
+        test1: { description: 'a', done: true, due_time: Time.now },
+        test2: { description: 'b', done: false },
+        test3: { description: 'c' },
+        test4: {},
+      }.stringify_keys
+      @bad_kvs_tasks = { '' => {} }
 
-  it 'adds a task with its description' do
-    should be_respond_to( :description )
-  end
+      @tasks = @kvs_tasks.map do|k,v|
+        Task.new( v.merge( name: k ) )
+      end
+    end
 
-  it 'adds a task whose description can be assigned' do
-    subject.description = 'This is a task for testing'
-    should be_valid
+    it 'can add multiple tasks with valid properties' do
+      returned = Task.add_from_kvs @kvs_tasks
+
+      Task.all.should eq @tasks
+    end
   end
 end
